346d2a60a4427339aa2f0a22d9b2bac0d8b6f882
Michal Karas <mjkaras93@gmail.com>
2025-04-01 22:10:22 +0200

    add top level directories to organise product structure

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,9 @@
+from src.main import sum_numbers
+
+
 class TestExample:
     def test_1_plus_1_equals_2(self):
         assert 1 + 1 == 2
+
+    def test_sum_numbers(self):
+        assert sum_numbers(1, 2) == 3
