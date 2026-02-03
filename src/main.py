f2e0f9ed9d51a9cca37b3efe08ae37cad95df873
MKaras93 <mjkaras93@gmail.com>
2026-02-03 22:02:44 +0100

    add logging configuration

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
+import logging
 from settings import settings
+
+settings.configure_logging()
+logger = logging.getLogger(__name__)
+
+# REPLACE CODE BELOW WITH YOUR APPLICATION LOGIC
 
 
 def sum_numbers(a, b):
     return a + b
 
 
-print(settings.settings_type)
+logger.info(f"Current settings type: {settings.settings_type}")
+logger.info(f"Sum of 3 and 5 is {sum_numbers(3, 5)}")
--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -4,7 +4,7 @@ Usage:
     from src.settings import settings
     print(settings.app_name)
 
-    You can add more envionrments using the same pattern.
+    You can add more environments using the same pattern.
 
 Explanation:
     Pydantic loads .env file after instantiating the Settings class.
@@ -12,8 +12,15 @@ Explanation:
     loading the .env file manually.
 """
 
+import datetime
+import logging
+import logging.config
 import os
+from pydantic import model_validator
 from pydantic_settings import BaseSettings
+
+# program run timestamp, meant to be static per program run (to separate logs and outputs per run)
+TIMESTAMP: str = datetime.datetime.now().strftime("%Y%m%d_%H-%M-%S-%f")
 
 
 class Settings(BaseSettings):
@@ -28,6 +35,103 @@ class Settings(BaseSettings):
         "env_file": ".env",
         "env_file_encoding": "utf-8",
     }
+
+    logs_dir: str = os.path.join(project_root, "local", "logs")
+    current_run_logs_dir: str = os.path.join(logs_dir, "runs", f"{TIMESTAMP}")
+    log_level: str | int = logging.INFO
+    logging_config: dict = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
+            "console": {
+                "format": "%(asctime)s [%(levelname)s] %(message)s",
+                "datefmt": "%H:%M:%S",
+            },
+        },
+        "handlers": {
+            "console": {
+                "level": log_level,
+                "formatter": "console",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+            "debug_file": {
+                "level": logging.DEBUG,
+                "formatter": "standard",
+                "class": "logging.FileHandler",
+                "filename": os.path.join(
+                    current_run_logs_dir,
+                    f"debug.log",
+                ),
+            },
+            "info_file": {
+                "level": logging.INFO,
+                "formatter": "console",
+                "class": "logging.FileHandler",
+                "filename": os.path.join(
+                    current_run_logs_dir,
+                    f"info.log",
+                ),
+            },
+            "current_info": {
+                "level": logging.INFO,
+                "formatter": "console",
+                "class": "logging.FileHandler",
+                "filename": os.path.join(logs_dir, "current_info.log"),
+                "mode": "w",
+            },
+            "current_debug": {
+                "level": logging.INFO,
+                "formatter": "standard",
+                "class": "logging.FileHandler",
+                "filename": os.path.join(logs_dir, "current_debug.log"),
+                "mode": "w",
+            },
+            # "debug_rotating": {  # for long running programs use this instead of debug_file
+            #     "level": logging.DEBUG,
+            #     "formatter": "standard",
+            #     "class": "logging.handlers.TimedRotatingFileHandler",
+            #     "filename": os.path.join(logs_dir, "debug.log"),
+            #     "when": "midnight",
+            #     "backupCount": 7,  # keep 7 days
+            #     "utc": True,
+            # },
+            # "info_rotating": {  # for long running programs use this instead of info_file
+            #     "level": logging.INFO,
+            #     "formatter": "console",
+            #     "class": "logging.handlers.TimedRotatingFileHandler",
+            #     "filename": os.path.join(logs_dir, "info.log"),
+            #     "when": "midnight",
+            #     "backupCount": 7,  # keep 7 days
+            #     "utc": True,
+            # },
+        },
+        "loggers": {
+            "": {  # root logger
+                "handlers": [
+                    "console",
+                    "debug_file",
+                    "info_file",
+                    "current_info",
+                    "current_debug",
+                ],
+                "level": logging.DEBUG,
+                "propagate": False,
+            },
+        },
+    }
+
+    def configure_logging(self):
+        """Configure logging based on settings.
+
+        This method should be ran once at the start of the application.
+        """
+        os.makedirs(self.current_run_logs_dir, exist_ok=True)
+        logging.config.dictConfig(self.logging_config)
+
+        logger = logging.getLogger(__name__)
+        logger.info(f"Logging configured. Logs directory: {self.logs_dir}")
 
 
 class TestSettings(Settings):
